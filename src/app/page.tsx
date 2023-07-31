2792cc3f599104b6ef7a666fa5ab6b7b3e3ba444
AugustoCVS <guty.vertolis@gmail.com>
2023-07-31 15:01:02 -0300

    feat: create video-player

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,15 @@
 
 import { Provider as ReduxProvider } from 'react-redux'
 
-import { AddTodo } from '@/components/AddTodo'
-import { TodoList } from '@/components/TodoList'
 import { store } from '@/store'
+import { Player } from '@/components/Player'
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="bg-gray-100 rounded-md shadow-md p-4">
+    <div>
+      <div>
         <ReduxProvider store={store}>
-          <TodoList />
-          <AddTodo />
+          <Player />
         </ReduxProvider>
       </div>
     </div>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,24 +1,12 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import { useSelector, TypedUseSelectorHook } from 'react-redux'
-
-const todoSlice = createSlice({
-  name: 'todo',
-  initialState: ['Fazer café', 'Estudar Redux', 'Estudar tailwind.css'],
-
-  reducers: {
-    add: (state, action) => {
-      state.push(action.payload.newTodo)
-    },
-  },
-})
+import { player } from './slices/player'
 
 export const store = configureStore({
   reducer: {
-    todo: todoSlice.reducer,
+    player,
   },
 })
-
-export const { add } = todoSlice.actions
 
 export type RootSate = ReturnType<typeof store.getState>
 export const useAppSelector: TypedUseSelectorHook<RootSate> = useSelector
